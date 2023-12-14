58fd60296b48d989e6d8c13073ae8fd176b43923
yenorii <144069387+Yenorii@users.noreply.github.com>
2023-12-13 20:50:58 -0500

    adding crowdfunding_db_schema.sql

---
+++ Resources/crowdfunding_db_schema.sql
@@ -0,0 +1,55 @@
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Contacts" (
+    "contact_id" varchar(10)   NOT NULL,
+    "first_name" varchar(50)   NOT NULL,
+    "last_name" varchar(50)   NOT NULL,
+    "email" varchar(50)   NOT NULL,
+    CONSTRAINT "pk_Contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+CREATE TABLE "Campaign" (
+    "cf_id" varchar(10)   NOT NULL,
+    "contact_id" varchar(10)   NOT NULL,
+    "company_name" varchar(250)   NOT NULL,
+    "description" varchar(250)   NOT NULL,
+    "goal" float   NOT NULL,
+    "pledged" float   NOT NULL,
+    "outcome" varchar(15)   NOT NULL,
+    "backers_count" int   NOT NULL,
+    "country" varchar(2)   NOT NULL,
+    "currency" varchar(3)   NOT NULL,
+    "launched_date" date   NOT NULL,
+    "end_date" date   NOT NULL,
+    "category_id" varchar(5)   NOT NULL,
+    "subcategory_id" varchar(10)   NOT NULL,
+    CONSTRAINT "pk_Campaign" PRIMARY KEY (
+        "cf_id"
+
+     )
+);
+
+CREATE TABLE "Category" (
+    "category_id" varchar(5)   NOT NULL,
+    "category" varchar(20)   NOT NULL,
+    CONSTRAINT "pk_Category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "Subcategory" (
+    "subcategory_id" varchar(10)   NOT NULL,
+    "subcategory" varchar(20)   NOT NULL,
+    CONSTRAINT "pk_Subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+select * from "Category"
+select * from "Subcategory"
+select * from "Contacts" limit 20
+select * from "Campaign" limit 20
